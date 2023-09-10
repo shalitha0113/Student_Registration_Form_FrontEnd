7b2a422eb8e6e85b6aefc3b496ecc07c0868a468
Shalitha <shalithamorapitiya@gmail.com>
2023-09-10 23:58:31 +0530

    Add update option

--- JS/app.js
+++ JS/app.js
@@ -8,7 +8,6 @@ function Student(firstName, lastName, email, dob, gender, address, telNo, batch)
     this.address = address;
     this.telNo = telNo;
     this.batch = batch;
-
 }
 
 function register() {
@@ -54,7 +53,7 @@ let tblBody = `<tr>
                     <td>Email</td>
                     <td>Address</td>
                     <td>Batch</td>
-                  </tr>`
+               </tr>`
 
 //Fetch(Get Request)
 let studentTable = document.getElementById("tblStudent2");
@@ -81,9 +80,10 @@ function loadtable() {
                     <td>${element.email}</td>
                     <td>${element.address}</td>
                     <td>${element.batch}</td>
-                    <td><button id="delete" onclick="deleteById(${element.id})" >Delete</button></td>
-                    <td><button onclick="updateById(${element.id})" >Edit</button></td>
                     <td><button onclick="viewById(${element.id})" >View</button></td>
+                    <td><button onclick="deleteById(${element.id})" ></button></td>
+                    <td><button onclick="updateById(${element.id})" ></button></td>
+                    
                   </tr>`
             })
             studentTable.innerHTML = tblBody;
@@ -110,11 +110,11 @@ function searchByName() {
 
     var myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
-  
+
     var requestOptions = {
-      method: "GET",
-      headers: myHeaders,
-      redirect: "follow",
+        method: "GET",
+        headers: myHeaders,
+        redirect: "follow",
     };
 
     let tBody = `<tr>
@@ -152,11 +152,11 @@ function searchByBatch() {
 
     var myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
-  
+
     var requestOptions = {
-      method: "GET",
-      headers: myHeaders,
-      redirect: "follow",
+        method: "GET",
+        headers: myHeaders,
+        redirect: "follow",
     };
 
     let tBody = `<tr>
@@ -179,7 +179,7 @@ function searchByBatch() {
                     <td>${element.batch}</td>
                     <td><button id="delete" onclick="deleteById(${element.id})" >Delete</button></td>
                     <td><button onclick="updateById(${element.id})" >Update</button></td>
-                    <td><button onclick="viewById(${element.id})" >View</button></td>
+                    <td><button id="view-form-id" onclick="viewById(${element.id})" >View</button></td>
                   </tr>`
             })
             studentTable.innerHTML = tBody;
@@ -205,6 +205,132 @@ function deleteById(id) {
         .then(result => location.reload())
         .catch(error => console.log('error', error));
 }
+
+
+// View Form
+
+let studentView = document.getElementById("tblView2");
+function viewById(id) {
+    let studentView = document.getElementById("tblView2");
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var requestOptions = {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow'
+    };
+
+    fetch(`http://localhost:8080/student/stu-id/${id}`, requestOptions)
+        .then(response => response.json())
+        .then(data => {
+            let tblBody2 = `
+                            <tr>
+                                <th><label >ID</label></th>  
+                                <th>${data.id}</th>
+                            </tr>
+                            <tr>
+                                <td><input type="text" id="firstName" value="${data.firstName}"></td>   
+                                <td><input type="text" id="lastName" value="${data.lastName}"></td>
+                            </tr>
+                            <tr>
+                                <th><label >Email</label></th>
+                                <td><input type="text" id="email" value="${data.email}"></td>
+                            </tr>
+                            <tr>
+                                <th><label >Birthday</label></th>
+                                <td><input type="text" id="dob" value="${data.dob}"></td>
+                            </tr>
+                            <tr>
+                                <th><label >Gender</label></th>
+                                <td><input type="text" id="gender" value="${data.gender}"></td>
+                            </tr>
+                            <tr>
+                                <th><label >Address</label></th>
+                                <td><input type="text" id="address" value="${data.address}"></td>
+                            </tr>
+                            <tr>
+                                <th><label >Telephone No</label></th>
+                                <td><input type="text" id="telNo" value="${data.telNo}"></td>
+                            </tr>
+                            <tr>
+                                <th><label >Batch</label></th>
+                                <td><input type="text" id="batch" value="${data.batch}"></td>
+                            </tr>
+                            <tr>
+                                <td><button onclick="deleteById(${data.id})" >Delete</button></td>
+                                <td><button onclick="updateById(${data.id})" >Update</button></td>
+                            </tr>`
+            studentView.innerHTML = tblBody2;
+        })
+
+        .catch(error => console.log('error', error));
+
+
+        /*View Form function */
+
+        document.querySelector("#view-form-id").addEventListener("click", function () {
+            document.querySelector(".view-form").classList.add("active");
+        });
+
+        document.querySelector(".view-form .close-btn").addEventListener("click", function () {
+            document.querySelector(".view-form").classList.remove("active");
+        });
+}
+
+//Update Option
+
+
+
+function updateById(id) {
+    let firstName = document.getElementById('firstName').value;
+    let lastName = document.getElementById('lastName').value;
+    let email = document.getElementById('email').value;
+    let dob = document.getElementById('dob').value;
+    let gender = document.getElementById("gender").value;
+    let address = document.getElementById('address').value;
+    let telNo = document.getElementById('telNo').value;
+    let batch = document.getElementById('batch').value;
+
+    
+
+    let student ={
+        firstName:firstName,
+        lastName:lastName,
+        email:email,
+        dob:dob,
+        gender:gender,
+        address:address,
+        telNo:telNo,
+        batch:batch
+    };
+
+    let studentJSON = JSON.stringify(student);
+    console.log(studentJSON);
+
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var requestOptions = {
+        method: 'PUT',
+        headers: myHeaders,
+        body: studentJSON,
+        redirect: 'follow'
+    };
+
+    fetch(`http://localhost:8080/student/${id}`, requestOptions)
+        .then(response => response.text())
+        .then(result => location.reload())
+        .catch(error => console.log('error', error));
+
+}
+
+
+
+
+
+
+
 
 
 
