5eb66fa4d6d7d6f4d49313784df9f69aa4eb572b
Shalitha <shalithamorapitiya@gmail.com>
2023-09-10 07:01:55 +0530

    Add Deleted option and developing search option

--- JS/app.js
+++ JS/app.js
@@ -1,13 +1,12 @@
-`use Strict`;
 
-function Student(firstName, lastName,email,dob,gender,address,telNo, batch) {
+function Student(firstName, lastName, email, dob, gender, address, telNo, batch) {
     this.firstName = firstName;
     this.lastName = lastName;
-    this.email=email;
-    this.dob=dob;
-    this.gender=gender;
-    this.address=address;
-    this.telNo=telNo;
+    this.email = email;
+    this.dob = dob;
+    this.gender = gender;
+    this.address = address;
+    this.telNo = telNo;
     this.batch = batch;
 
 }
@@ -15,20 +14,20 @@ function Student(firstName, lastName,email,dob,gender,address,telNo, batch) {
 function register() {
     const fname = document.getElementById("firstName").value;
     const lname = document.getElementById("lastName").value;
-    const email=document.getElementById("email").value;
+    const email = document.getElementById("email").value;
     const dob = document.getElementById("dob").value;
-    const gender=document.getElementById("gender").value;
-    const address=document.getElementById("address").value;
-    const telNo=document.getElementById("telNo").value;
+    const gender = document.getElementById("gender").value;
+    const address = document.getElementById("address").value;
+    const telNo = document.getElementById("telNo").value;
     const batch = document.getElementById("batch").value;
 
     //Creating JSON object
-    const student = JSON.stringify(new Student(fname, lname,email,dob,gender,address,telNo,batch));
+    const student = JSON.stringify(new Student(fname, lname, email, dob, gender, address, telNo, batch));
     console.log(student);
 
     //Fetch (POST Request)
-    //var myHeaders = new Headers();
-    new Headers().append("Content-Type", "application/json");
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
 
     var requestOptions = {
         method: 'POST',
@@ -47,42 +46,122 @@ function register() {
 //------------------Student View Form --------------------------------------------------
 //let studentTable = document.getElementById("tbl-student");
 
-//Fetch(Get Request)
-let studentTable=document.getElementById("tbl-student");
-fetch("http://localhost:8080/student")
-.then(response=>response.json())
-.then(res=>{
-    let tblBody=`<tr>
-                    <th>ID</th>
-                    <th>Name</th>
-                    <th>Email</th>
-                   
-                    <th>Address</th>
-                   
-                    <th>Batch</th>
-                    
+
+//Table Body
+let tblBody = `<tr>
+                    <td>ID</td>
+                    <td>Name</td>
+                    <td>Email</td>
+                    <td>Address</td>
+                    <td>Batch</td>
                   </tr>`
-    res.forEach(element =>{
-        tblBody+=`<tr>
+
+//Fetch(Get Request)
+let studentTable = document.getElementById("tblStudent2");
+
+function loadtable() {
+    let studentTable = document.getElementById("tblStudent2");
+
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var requestOptions = {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow'
+    };
+
+    fetch("http://localhost:8080/student", requestOptions)
+        .then(response => response.json())
+        .then(result => {
+            result.forEach((element) => {
+                tblBody += `<tr>
                     <td>${element.id}</td>
                     <td>${element.firstName} ${element.lastName}</td>
                     <td>${element.email}</td>
-           
                     <td>${element.address}</td>
-                
                     <td>${element.batch}</td>
-                    
+                    <td><button id="delete" onclick="deleteById(${element.id})" >Delete</button></td>
+                    <td><button onclick="updateById(${element.id})" >Edit</button></td>
+                    <td><button onclick="viewById(${element.id})" >View</button></td>
                   </tr>`
-    })
-    studentTable.innerHTML=tblBody;
+            })
+            studentTable.innerHTML = tblBody;
+        })
+        .catch(error => console.log('error', error));
+}
 
-})
+loadtable();
 
-/*Login form view */ 
-document.querySelector("#register-btn").addEventListener("click",function(){
+/*Login form view */
+document.querySelector("#register-btn").addEventListener("click", function () {
     document.querySelector(".popup-register").classList.add("active");
 });
 
-document.querySelector(".popup-register .close-btn").addEventListener("click",function(){
+document.querySelector(".popup-register .close-btn").addEventListener("click", function () {
     document.querySelector(".popup-register").classList.remove("active");
 });
+
+
+/* Search function  */
+function search() {
+    let searchVal = document.getElementById("searchTxt").value;
+    alert(searchVal);
+
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+  
+    var requestOptions = {
+      method: "GET",
+      headers: myHeaders,
+      redirect: "follow",
+    };
+
+    let tBody = `<tr>
+                    <td>ID</td>
+                    <td>Name</td>
+                    <td>Email</td>
+                    <td>Address</td>
+                    <td>Batch</td>
+                  </tr>`
+
+    fetch(`http://localhost:8080/student/${searchVal}`, requestOptions)
+        .then(response => response.json())
+        .then(result => {
+            result.forEach((element) => {
+                tBody += `<tr>
+                    <td>${element.id}</td>
+                    <td>${element.firstName} ${element.lastName}</td>
+                    <td>${element.email}</td>
+                    <td>${element.address}</td>
+                    <td>${element.batch}</td>
+                    <td><button id="delete" onclick="deleteById(${element.id})" >Delete</button></td>
+                    <td><button onclick="updateById(${element.id})" >Update</button></td>
+                    <td><button onclick="viewById(${element.id})" >View</button></td>
+                  </tr>`
+            })
+            studentTable.innerHTML = tBody;
+        })
+        .catch(error => console.log('error', error));
+}
+
+
+/*Delete Option */
+function deleteById(id) {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var requestOptions = {
+        method: 'DELETE',
+        headers: myHeaders,
+        redirect: 'follow'
+    };
+
+    fetch(`http://localhost:8080/student/${id}`, requestOptions)
+        .then(response => response.text())
+        .then(result => location.reload())
+        .catch(error => console.log('error', error));
+}
+
+
+
